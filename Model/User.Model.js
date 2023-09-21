0a48748f5253ba23a4d2a2daaa9382b2c6ea0e80
arpit2444 <arpitjain2444@gmail.com>
2023-09-21 12:32:16 +0530

    A

--- Model/User.Model.js
+++ Model/User.Model.js
@@ -1,13 +1,8 @@
 const mongoose=require("mongoose")
 
 const userSchema = mongoose.Schema({
-    first_name:{
+    fname:{
         type:String,
-        required:true
-    },
-    last_name:{
-        type:String,
-        required:true
     },
     email:{
         type:String,
