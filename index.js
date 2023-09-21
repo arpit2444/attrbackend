30351aeae8b471d84dc47ba4a0b2ec02b443a9b7
arpit2444 <arpitjain2444@gmail.com>
2023-09-21 12:18:39 +0530

    A

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use("/users", userRouter);
+app.use("/user", userRouter);
 app.use("/oem",oemRouter)
 app.use("/marketplace",MarketPlaceRouter)
 
