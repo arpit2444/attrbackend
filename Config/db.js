1e9e6540c0f36d8d20cdf50c49aea42a54819b91
arpit2444 <arpitjain2444@gmail.com>
2023-09-21 09:32:53 +0530

    A

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require('dotenv').config()
 
-require("dotenv").config()
 
 const connection = mongoose.connect(`${process.env.MONGOURL}/buycars`)
 
-module.exports={connection}
+module.exports={
+    connection
+}
