9e5cbaef04f17b7033d672f5def955bad55ac02b
arpit2444 <arpitjain2444@gmail.com>
2023-09-21 09:07:43 +0530

    A

---
+++ Model/User.Model.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+
+const userSchema = mongoose.Schema({
+    first_name:{
+        type:String,
+        required:true
+    },
+    last_name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+})
+
+const UserModel = mongoose.model("User",userSchema)
+
+module.exports={UserModel}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors")
+const {connection}= require("./Config/db")
+const { userRouter } = require("./Routes/User.Route");
+const { oemRouter } = require("./Routes/Oem.Route");
+const { MarketPlaceRouter } = require("./Routes/Marketplace_Inventory.Route");
+require("dotenv").config()
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use("/users", userRouter);
+app.use("/oem",oemRouter)
+app.use("/marketplace",MarketPlaceRouter)
+
+const PORT= process.env.port
+
+app.listen(PORT, async () => {
+    try {
+      await connection;
+      console.log("Connected to DB");
+    } catch (error) {
+      console.log(" Cannot Connected to DB");
+      console.log(error);
+    }
+    console.log("Server is running");
+  });
